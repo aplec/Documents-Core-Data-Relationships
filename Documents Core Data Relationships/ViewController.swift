69703af6af0c3cb65e311ffc1823d055dffe0d33
Ante Plecas <anteplecas@Antes-MacBook-Pro.local>
2020-02-28 17:56:26 -0600

    First commit

---
+++ Documents Core Data Relationships/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Documents Core Data Relationships
+//
+//  Created by Ante Plecas on 2/28/20.
+//  Copyright © 2020 Ante Plecas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Documents Core Data RelationshipsTests/Documents_Core_Data_RelationshipsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Documents_Core_Data_RelationshipsTests.swift
+//  Documents Core Data RelationshipsTests
+//
+//  Created by Ante Plecas on 2/28/20.
+//  Copyright © 2020 Ante Plecas. All rights reserved.
+//
+
+import XCTest
+@testable import Documents_Core_Data_Relationships
+
+class Documents_Core_Data_RelationshipsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
